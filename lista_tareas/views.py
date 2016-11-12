cf183185851f9797a30f5acbe24e9bb28940bf4f
Alan Boglioli <kiriost.s@gmail.com>
2016-11-12 04:03:56 -0300

    Agregada plantilla y nueva página

--- lista_tareas/views.py
+++ lista_tareas/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Tarea
 
-# Create your views here.
+def lista_tareas(request):
+    tareas = Tarea.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'lista_tareas/lista_tareas.html', {'tareas': tareas})
